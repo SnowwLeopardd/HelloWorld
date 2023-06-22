49540d55ee773016f064d31f6209dc2095f59d25
AleksandrBochkarev <snoww.leopardd@gmail.com>
2023-06-22 11:05:44 -1000

    1. Traffic light is ready

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+    
+    @IBOutlet var redCircle: UIView!
+    @IBOutlet var yellowCircle: UIView!
+    @IBOutlet var greenCircle: UIView!
 
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red // Необходимо для захождения в switch
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        startButton.layer.cornerRadius = 10
+        
+        redCircle.alpha = lightIsOff
+        yellowCircle.alpha = lightIsOff
+        greenCircle.alpha = lightIsOff
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        redCircle.layer.cornerRadius = redCircle.frame.width/2
+        yellowCircle.layer.cornerRadius = yellowCircle.frame.width/2
+        greenCircle.layer.cornerRadius = greenCircle.frame.width/2
+    }
+    
+    @IBAction func startButtonTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenCircle.alpha = lightIsOff
+            redCircle.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redCircle.alpha = lightIsOff
+            yellowCircle.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenCircle.alpha = lightIsOn
+            yellowCircle.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
 }
 
+  
